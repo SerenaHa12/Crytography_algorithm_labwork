300c0ca09f92a22a5fa8bdc0b0ec000ba5b27386
SerenaHa12 <serenavander.ha@gmail.com>
2023-02-02 09:34:17 +0700

    labwork

---
+++ ex1.cpp
@@ -0,0 +1,3 @@
+//A5/1 is a stream cipher used to provide over-the-air communication privacy in the GSM cellular telephone standard.
+
+//A5/1 is based around a combination of three linear feedback shift registers (LFSRs) with irregular clocking.
